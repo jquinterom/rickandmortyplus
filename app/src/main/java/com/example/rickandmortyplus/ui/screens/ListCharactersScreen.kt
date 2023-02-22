5ac10ad3cf3a159a29129052d9432af32da1b42e
JhonQuintero <jfqm7710@gmail.com>
2023-02-22 17:41:30 -0500

    Update for characters

--- app/src/main/java/com/example/rickandmortyplus/ui/screens/ListCharactersScreen.kt
+++ app/src/main/java/com/example/rickandmortyplus/ui/screens/ListCharactersScreen.kt
@@ -1,11 +1,17 @@
 package com.example.rickandmortyplus.ui.screens
 
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import com.example.rickandmortyplus.ui.composables.CharacterCard
 
 
 @Composable
 fun ListCharactersScreen() {
-    Text(text = "List Characters")
+    LazyColumn(){
+        items(8){
+            CharacterCard()
+        }
+    }
 }
 
