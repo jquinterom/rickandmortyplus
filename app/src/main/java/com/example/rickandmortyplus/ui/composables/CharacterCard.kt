fcc9790aa8a901a926b89d32b1daa6e172f40441
-jquinterom <-jfqm7710@gmail.com>
2023-02-22 10:11:43 -0500

    Update gradle and dependecnies version

--- app/src/main/java/com/example/rickandmortyplus/ui/composables/CharacterCard.kt
+++ app/src/main/java/com/example/rickandmortyplus/ui/composables/CharacterCard.kt
@@ -7,11 +7,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun CharacterCard() {
     Card(
-        modifier = Modifier.shadow(dimensionResource(id = ))
+        modifier = Modifier.shadow(8.dp)
     ) {
         Text(text = "Character card")
     }
